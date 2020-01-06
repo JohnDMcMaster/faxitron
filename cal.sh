93f02c286f0de74969d0f5c31f9d531bb3ec6ece
John McMaster <johndmcmaster@gmail.com>
2020-01-05 18:20:12 -0800

    cal.sh: correct default env syntax
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- cal.sh
+++ cal.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-n=${CALN,32}
+n=${CALN:-32}
 d=$(python3 cal_dir.py)
 echo "Output dir $d: writing $n files"
 mkdir -p $d
-python3 main.py --kvp 0 -n $n --dir $d/df --raw
-python3 main.py --kvp 35 -n $n --dir $d/ff --raw
+python3 main.py --kvp 0 -n "$n" --dir "$d/df" --raw
+python3 main.py --kvp 35 -n "$n" --dir "$d/ff" --raw
 python3 cal.py $d/ff $d/df $d
 
